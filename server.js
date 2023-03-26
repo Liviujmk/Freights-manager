e0a0d5a52c5c2c03df041fa11534d7bb6abf9322
admin <admin@DESKTOP-6D3IIJS>
2023-03-27 00:11:24 +0300

    final update on styling

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require('express');
+const express = require( 'express' );
 const app = express();
 
 //dotnev
-require('dotenv').config();
+require( 'dotenv' ).config();
 
 //connect to mongo db
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => {
-    console.log("MongoDB connected");
-})
-.catch(err => console.log(err));
+const mongoose = require( 'mongoose' );
+mongoose.connect( process.env.DB_STRING, { useNewUrlParser: true, useUnifiedTopology: true } )
+    .then( () => {
+        console.log( "MongoDB connected" );
+    } )
+    .catch( err => console.log( err ) );
 
 //set ejs
-app.set('view engine', 'ejs');
+app.set( 'view engine', 'ejs' );
 
 //url encoded
-app.use(express.urlencoded({ extended: true }));
+app.use( express.urlencoded( { extended: true } ) );
 
 // expres static
-app.use('/static', express.static('static'))
+app.use( '/static', express.static( 'static' ) )
 
 
 //import routes
-const routes = require('./routes/routes.js');
-app.use('/', routes);
+const routes = require( './routes/routes.js' );
+app.use( '/', routes );
 
 //listen server
-app.listen(process.env.PORT || 5050, () => console.log(`Example app listening on port 5050!`));
+app.listen( process.env.PORT || 5050, () => console.log( `Example app listening on port 5050!` ) );
